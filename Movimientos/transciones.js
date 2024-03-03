976ca571b47944d61f46fb7e9af2ca415fbb23bc
VanessaRivas2000 <rivasaraujojacqueline@gmail.com>
2024-03-03 11:31:22 -0600

    mas cambioss

--- Movimientos/transciones.js
+++ Movimientos/transciones.js
@@ -1,17 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cards = document.querySelectorAll(".card");
-  
-    // Agregar evento de mouseover para animación de aumento
-    cards.forEach(card => {
-      card.addEventListener("mouseover", function() {
-        this.style.transform = "scale(1.1)";
-        this.style.transition = "transform 0.3s ease";
-      });
-  
-      // Agregar evento de mouseout para animación de retorno al tamaño original
-      card.addEventListener("mouseout", function() {
-        this.style.transform = "scale(1)";
-      });
-    });
-  });
-  
+function flipCard(card) {
+  card.classList.toggle('flipped');
+}
